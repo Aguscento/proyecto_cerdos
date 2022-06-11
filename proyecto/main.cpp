3932176c7f2632921d4a2035726b6e96d6dfac74
Aguscento <agus_cento@hotmail.com>
2022-06-10 23:34:17 -0300

    teste

--- proyecto/main.cpp
+++ proyecto/main.cpp
@@ -18,7 +18,7 @@ int main(){
     int TrufasJ1 = 0, TrufasJ2 = 0;                 // contador de trufas
     for (int i = 0; i < 5; ++i) {                   // 5 rondas en total
         char continuar;                           // char para colocar S o N
-        do {
+        do {                                        // while para lanzar otra vez
             juego(jugador1, jugador2, TrufasJ1, TrufasJ2, jugador1 ); // turno de jugador 1
             cout << "Continuar? (S/N)" << endl;
             cin >> continuar;
